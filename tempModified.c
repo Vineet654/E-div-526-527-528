42483703d081bdb9feb3d523f2a738d17c357221
Vineet654 <131142975+Vineet654@users.noreply.github.com>
2024-01-22 02:32:27 +0530

    Update tempModified.c

--- tempModified.c
+++ tempModified.c
@@ -238,7 +238,17 @@ switch (choice)
                     scanf("%d", &source);
                     Dijkstra(graph, 5, source, city);
                     break;
-                case 3:
+                case 3: printf("Enter graph file: ");
+                    scanf("%s", gfname);
+                    FILE *fp3 = fopen(gfname, "r");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        for (int j = 0; j < 5; j++)
+                            fscanf(fp2, "%d", &graph[i][j]);
+                    }
+                    fclose(fp2);
+
+                    kruskalAlgo(5, graph);
                     break;
                 case 4:
                     printf("Sort cities based on:\n");
@@ -637,3 +647,88 @@ void readFile(char fname [],char content [])
     fclose(fp);
 }
 
+int comparator(const void* p1, const void* p2)
+{
+    const int(*x)[3] = p1;
+    const int(*y)[3] = p2;
+
+    return (*x)[2] - (*y)[2];
+}
+
+// Initialization of parent[] and rank[] arrays
+void makeSet(int parent[], int rank[], int n)
+{
+    for (int i = 0; i < n; i++) {
+        parent[i] = i;
+        rank[i] = 0;
+    }
+}
+
+// Function to find the parent of a node
+int findParent(int parent[], int component)
+{
+    if (parent[component] == component)
+        return component;
+
+    return parent[component]
+           = findParent(parent, parent[component]);
+}
+
+// Function to unite two sets
+void unionSet(int u, int v, int parent[], int rank[], int n)
+{
+    // Finding the parents
+    u = findParent(parent, u);
+    v = findParent(parent, v);
+
+    if (rank[u] < rank[v]) {
+        parent[u] = v;
+    }
+    else if (rank[u] > rank[v]) {
+        parent[v] = u;
+    }
+    else {
+        parent[v] = u;
+
+        // Since the rank increases if
+        // the ranks of two sets are same
+        rank[u]++;
+    }
+}
+
+// Function to find the MST
+void kruskalAlgo(int n, int edge[n][3])
+{
+    // First we sort the edge array in ascending order
+    // so that we can access minimum distances/cost
+    qsort(edge, n, sizeof(edge[0]), comparator);
+
+    int parent[n];
+    int rank[n];
+
+    // Function to initialize parent[] and rank[]
+    makeSet(parent, rank, n);
+
+    // To store the minimun cost
+    int minCost = 0;
+
+    printf(
+        "Following are the edges in the constructed MST\n");
+    for (int i = 0; i < n; i++) {
+        int v1 = findParent(parent, edge[i][0]);
+        int v2 = findParent(parent, edge[i][1]);
+        int wt = edge[i][2];
+
+        // If the parents are different that
+        // means they are in different sets so
+        // union them
+        if (v1 != v2) {
+            unionSet(v1, v2, parent, rank, n);
+            minCost += wt;
+            printf("%d -- %d == %d\n", edge[i][0],
+                   edge[i][1], wt);
+        }
+    }
+
+    printf("Minimum Cost Spanning Tree: %d\n", minCost);
+}
